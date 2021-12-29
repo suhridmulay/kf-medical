aab1671cec0a65f2a1a7b912e0603d67cd5f3f85
Dhritiman Banerjee <banerjee.dhritiman@gmail.com>
2021-12-29 13:09:49 -0500

    Fixing incorrect service URL

--- frontend/src/config/production.tsx
+++ frontend/src/config/production.tsx
@@ -1,6 +1,6 @@
 // Place the URL here with the /api suffix.
 // Ex.:`https://domain.com/api`;
-const backendUrl = `https://health-service-dot-kf-apps-288002.uc.r.appspot.com/api`;
+const backendUrl = `https://medical-service-dot-kf-apps-288002.uc.r.appspot.com/api`;
 
 // SwaggerUI Documentation URL
 // Leave black if documentation should be hidden
