0e5b6c59874e6567cfdb6cb3c23dae5caa7324d5
Dhritiman Banerjee <banerjee.dhritiman@gmail.com>
2022-05-16 21:28:46 -0400

    Rural doctors are restricted to see their own patients
    Only telemed doctors get to see details of all patients

--- backend/src/api/patient/patientList.ts
+++ backend/src/api/patient/patientList.ts
@@ -1,13 +1,29 @@
 import PermissionChecker from '../../services/user/permissionChecker';
+import Roles from '../../security/roles';
 import ApiResponseHandler from '../apiResponseHandler';
 import Permissions from '../../security/permissions';
 import PatientService from '../../services/patientService';
+import DoctorService from '../../services/doctorService';
 
 export default async (req, res, next) => {
   try {
-    new PermissionChecker(req).validateHas(
+    const permissions = new PermissionChecker(req);
+    permissions.validateHas(
       Permissions.values.patientRead,
     );
+    // Rural village doctors can only see their local patients - only telemedicine doctors
+    // get to see all patients. Get the location affiliated with the doctor
+    const currentUserRoles = permissions.currentUserRolesIds;
+    if (currentUserRoles[0] === Roles.values.doctor) {
+      const filter = {"user": req.currentUser.id};
+      const doctor = await new DoctorService(req).findAndCountAll(
+         {filter},
+      );
+      const affiliatedMedicalCenter = doctor.rows[0].medicalCenterId;
+      if (affiliatedMedicalCenter) {
+        req.query.filter['medicalCenter'] = affiliatedMedicalCenter;
+      }
+    }
 
     const payload = await new PatientService(
       req,
