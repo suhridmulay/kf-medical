8538eb21f53b35772962d12f7794c9894d41edc9
Dhritiman Banerjee <banerjee.dhritiman@gmail.com>
2022-09-04 11:49:33 -0400

    Fix up inventories at source and destination

--- backend/src/services/transferService.ts
+++ backend/src/services/transferService.ts
@@ -4,6 +4,8 @@ import { IServiceOptions } from './IServiceOptions';
 import TransferRepository from '../database/repositories/transferRepository';
 import HealthCenterRepository from '../database/repositories/healthCenterRepository';
 import MedicineBatchRepository from '../database/repositories/medicineBatchRepository';
+import SiteInventoryRepository from '../database/repositories/siteInventoryRepository';
+import { and } from 'sequelize/types';
 
 export default class TransferService {
   options: IServiceOptions;
@@ -26,11 +28,56 @@ export default class TransferService {
         ...this.options,
         transaction,
       });
+      
+      // Fix up the site inventories - decrease at source and increase at destination
+      let inventoryAtSourceCenter = await SiteInventoryRepository.findAndCountAll({filter: {center: data.fromCenter, batchNumber: data.medicineBatch}}, this.options);
+      let inventoryAtDestCenter = await SiteInventoryRepository.findAndCountAll({filter: {center: data.toCenter, batchNumber: data.medicineBatch}}, this.options);
 
+      if (inventoryAtSourceCenter.count > 0) {
+        let srcInventoryRecord = inventoryAtSourceCenter.rows[0];
+        let toCenterDetails = await HealthCenterRepository.findById(data.toCenter, this.options);
+        let medicineBatchDetails = await MedicineBatchRepository.findById(data.medicineBatch, this.options);
+
+        if (inventoryAtDestCenter.count == 0) { // create a new entry
+          let inventoryData = {
+            center: data.toCenter,
+            medicine: srcInventoryRecord.medicineId,
+            batchNumber: srcInventoryRecord.batchNumberId,
+            expiryDate: srcInventoryRecord.expiryDate,
+            inventoryAddDate: data.transferDate,
+            initialCount: data.transferQuantity,
+            currentCount: data.transferQuantity,
+            siteBatchIdentifier: toCenterDetails.name + " | " + medicineBatchDetails.medicineBatchLookup,
+          };
+          const inventoryRecord = await SiteInventoryRepository.create(inventoryData, {
+            ...this.options,
+            transaction,
+          });
+        } else { // Update the record found
+          let destInventoryRecord = inventoryAtDestCenter.rows[0];
+          destInventoryRecord.currentCount += data.transferQuanity;
+
+          // Updates require the IDs as opposed to the expanded record 
+          destInventoryRecord.medicine = destInventoryRecord.medicineId;
+          destInventoryRecord.batchNumber = destInventoryRecord.batchNumberId;
+          destInventoryRecord.center = destInventoryRecord.centerId;
+
+          let result2 = await SiteInventoryRepository.update(destInventoryRecord.id, destInventoryRecord, {...this.options, transaction,});
+        }
+        
+        srcInventoryRecord.currentCount -= data.transferQuantity;
+
+        // Updates require the IDs as opposed to the expanded record 
+        srcInventoryRecord.medicine = srcInventoryRecord.medicineId;
+        srcInventoryRecord.batchNumber = srcInventoryRecord.batchNumberId;
+        srcInventoryRecord.center = srcInventoryRecord.centerId;
+
+        let result1 = await SiteInventoryRepository.update(srcInventoryRecord.id, srcInventoryRecord, {...this.options, transaction,});
+      }
       await SequelizeRepository.commitTransaction(
         transaction,
       );
-
+    
       return record;
     } catch (error) {
       await SequelizeRepository.rollbackTransaction(
